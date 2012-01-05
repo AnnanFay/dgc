65894ffe69c699fa29a56526f61fcf06088a9d25
Annan Fay Yearian <annanfay@gmail.com>
2012-01-05 18:24:58 +0000

    added deps and main

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject DGC "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                  [seesaw "1.3.0"]]
+  :main DGC.core)
