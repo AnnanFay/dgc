20ecdc86f24bb5548276def1ef9b4d8c494ed894
Annan Yearian <annanfay@gmail.com>
2012-01-15 23:51:21 +0000

    slurping xml

--- src/DGC/core.clj
+++ src/DGC/core.clj
@@ -4,16 +4,6 @@
         seesaw.color
         [cheshire.core]))
 
-(defn make-table-example []
-  (table :id :table
-    :model [
-      :columns [ { :key :name :text "Name" } 
-                 { :key :town :text "Town" } 
-                 { :key :interest :text "Interest" }]
-      :rows [{ :name "Kupzog" :town "Cologne" :interest "programming" :id 1234}
-             { :name "Hansson" :town "Ystadt" :interest "Hunting" :id 2234}
-             { :name "Walter" :town "London" :interest "Rafting" :id 12345}]]))
-
 (defn header [k]
   {:key k :text (name k)})
 
@@ -35,18 +25,15 @@
 (defn -main
   []
   (let [height 400
-        data (parse-string (slurp "Dwarves.json") true)
-        test-data [{ :name "Kupzog" :town "Cologne" :interest "programming" :id 1234}
-                   { :name "Hansson" :town "Ystadt" :interest "Hunting" :id 2234}
-                   { :name "Walter" :town "London" :interest "Rafting" :id 12345}]]
+        data (parse-string (slurp "Dwarves.json") true)]
     (-> (frame 
           :title    "Dwarven Guidance Councilor" 
           :height   height
           :width    (* height 1.618)
           :content  (border-panel
                       ;:north (text :multi-line? true :wrap-lines? false :rows 3 :text (str data))
-                      :north  (scrollable (make-table test-data to-dwarf))
-                      :center (scrollable (make-table data to-info))
+                      :north (scrollable (make-table data to-info))
+                      :center (scrollable (make-table data to-dwarf))
                       :south  (label :id :sel :text "Selection: "))
           :on-close :exit)
         pack!
