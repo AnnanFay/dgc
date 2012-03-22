560e72de6b155447e36eeb46292420a5f5aee245
Annan Yearian <annanfay@gmail.com>
2012-03-22 00:20:32 +0000

    upgraded seesaw dep, added protobuf dep and fixed java-source-path

--- project.clj
+++ project.clj
@@ -1,10 +1,13 @@
 (defproject dgc "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [seesaw "1.3.0"]
+                 [seesaw "1.4.0"]
                  [cheshire "2.0.4"]
+                 [clojure-protobuf "0.4.7-SNAPSHOT"]
+                 ;[me.hspy/defrecord2 "1.0.0-SNAPSHOT"]
                  ;[org.jfree/jfreechart "1.0.14"]
                  ;[incanter/incanter-charts "1.3.0-SNAPSHOT"]
                  ]
+ 	:dev-dependencies [[protobuf "0.6.0-beta8"]]
   :main dgc.core
-  :java-source-path [["src/java"]])
+  :java-source-path "src/java")
